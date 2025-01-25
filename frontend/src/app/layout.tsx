f58f004d608c5ad955d33085c1f05f4c7090e5ec
sandeep <sandeepsanu1230@gmail.com>
2025-01-25 18:04:07 +0530

    updated layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import TodoNavbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 export const metadata = {
   title: "Task Reminder",
@@ -13,6 +14,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="antialiased flex flex-col min-h-screen">
+        <Head>
+          <link rel="icon" href="/todo.png" type="image/png" />
+        </Head>
         <TodoNavbar />
         <main className="flex-grow">
           {children}
