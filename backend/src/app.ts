d973fa1bd962c2862fb09ed2c784771fb7a4de28
sandeep <sandeepsanu1230@gmail.com>
2025-01-25 15:13:29 +0530

    updated cors options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,9 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000", 
-  credentials: true, 
-  methods: ["GET", "POST", "PUT", "DELETE"], 
+  origin: "https://taskhive-manage-your-tasks.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
