156430f8e49b6631a4792f4111753fcf83433c1e
sandeep <sandeepsanu1230@gmail.com>
2025-04-16 15:04:15 +0530

    updated authRoutes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authController } from "../controllers/authController";
 import { rateLimiter } from "../middlewares/rateLimiter";
+import { upload } from "../middlewares/uploadMiddleware";
 
 const router = Router();
 
-router.post("/signup", authController.signUp);
+router.post("/signup",upload.single("profilePhoto"), authController.signUp);
 router.post("/login", rateLimiter, authController.login);
 router.post("/logout", authController.logout);
 
